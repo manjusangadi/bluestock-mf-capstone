3e671082aa3acf35a1434645e09e93408058d51c
unknown <angadimanju052@gmail.com>
2026-06-03 23:05:48 +0530

    add live nav file

---
+++ live_nav_fetch.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+
+codes = {
+    "HDFC_Top100": 125497,
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "Nippon_LargeCap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+for fund, code in codes.items():
+
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    nav_df = pd.DataFrame(data["data"])
+
+    nav_df.to_csv(
+        f"data/raw/live_nav_{code}.csv",
+        index=False
+    )
+
+    print(f"live_nav_{code} NAV data saved successfully")
